f646a97527871c66c56c8a2041b5c236cbc4b318
SINOVIA <somayfield@gmail.com>
2016-06-05 23:10:53 -0400

    Returning Icon Values

--- interaction.js
+++ interaction.js
@@ -23,29 +23,47 @@ var minutesLabel = document.getElementById("minutes");
            }
        };
 // CARD FUNCTIONS
-       (function() {
-         var cards = document.querySelectorAll(".card.effect__click");
-         for ( var i  = 0, len = cards.length; i < len; i++ ) {
-           var card = cards[i];
-           clickListener( card );
-         }
-          var track = 0
-         function clickListener(card) {
-           card.addEventListener( "click", function() {
-             track++
-             if (track === 1){
-             var c = this.classList;
-             c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped","active");
-             console.log("flippage");
+(function() {
+var cards = document.querySelectorAll(".card.effect__click");
+for ( var i  = 0, len = cards.length; i < len; i++ ) {
+var card = cards[i];
+clickListener( card );
+}
+var track = 0
+function clickListener(card) {
+card.addEventListener( "click", function() {
+  track++
+  if (track === 1){
+  var c = this.classList;
+  c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped","active");
+    var cardIcon1 = (this.querySelectorAll(".fa"))
+      var flippedIcon1= cardIcon1;
+            console.log(flippedIcon1);
+            return flippedIcon1
 
-           } else if (track === 2) {
-              console.log("whomp")
-            }
+if (flippedIcon1 === flippedIcon2) {
+  console.log("YAY!")
+}
 
-               });
-         }
-       })();
 
+} else if (track === 2) {
+  var c = this.classList;
+c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped","active");
+  var cardIcon2 = (this.querySelectorAll(".fa"))
+  var flippedIcon2= cardIcon2;
+
+    console.log(flippedIcon2);
+    return flippedIcon2;
+
+}
+
+    });
+
+
+}
+})();
+
+// LIFEBAR
 var count = 0
 $(".takeALife").click(function(){
   count++;
